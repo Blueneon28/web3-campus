998cabc060fe42b84c77dce31c8daf42ec2a0628
jasonhermawan <jasonhs.dev@gmail.com>
2025-06-15 13:09:54 +0700

    feat: add merchant withdrawal functionality

--- front-end/src/features/merchant/withdraw/index.tsx
+++ front-end/src/features/merchant/withdraw/index.tsx
@@ -1,7 +1,13 @@
+import { Box } from "@mantine/core";
 import React from "react";
+import WithdrawForm from "./components/WithdrawForm";
 
 const MerchantDashboardWithdraw = () => {
-  return <div>Withdraw</div>;
+  return (
+    <Box w="65%" m="auto">
+      <WithdrawForm />
+    </Box>
+  );
 };
 
 export default MerchantDashboardWithdraw;
