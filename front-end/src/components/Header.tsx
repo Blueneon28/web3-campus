3e60ff51b3e6cc577a60d9f5d9d5ed0030b5a9f3
jasonhermawan <jasonhs.dev@gmail.com>
2025-06-20 07:16:47 +0700

    feat: get user role for header links

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,12 +1,52 @@
+"use client";
 import { Box, Flex, Text } from "@mantine/core";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAccount, useReadContract } from "wagmi";
+import { shortenAddress } from "@/utils/helpers";
+import { contracts } from "@/constants/contracts";
 
 interface IHeaderProps {
   withBorder: boolean;
 }
 
 const Header: React.FC<IHeaderProps> = ({ withBorder }) => {
+  const { address, isConnected } = useAccount();
+  const [role, setRole] = useState<string>("");
+  const { studentID, campusCredit } = contracts;
+
+  const { data: detailStudent } = useReadContract({
+    address: studentID.address,
+    abi: studentID.abi,
+    functionName: "getStudentByAddress",
+    args: address ? [address] : undefined,
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  const { data: detailMerchant } = useReadContract({
+    address: campusCredit.address,
+    abi: campusCredit.abi,
+    functionName: "isMerchant",
+    args: address ? [address] : undefined,
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  useEffect(() => {
+    if (isConnected) {
+      if (detailStudent) {
+        setRole("Student");
+      } else if (detailMerchant) {
+        setRole("Merchant");
+      }
+    } else {
+      setRole("");
+    }
+  }, [isConnected, detailStudent, detailMerchant]);
+
   return (
     <Box
       py="18px"
@@ -14,7 +54,18 @@ const Header: React.FC<IHeaderProps> = ({ withBorder }) => {
     >
       <Flex className="layout" align="center" justify="space-between">
         <Text>Web3 Campus</Text>
-        <ConnectButton />
+        <Flex align="center" gap="4px">
+          {role === "Student" ? (
+            <Text>Student Dashboard</Text>
+          ) : role === "Merchant" ? (
+            <Text>Merchant Dashboard</Text>
+          ) : role === "Admin" ? (
+            <Text>Admin Dashboard</Text>
+          ) : (
+            <></>
+          )}
+          <ConnectButton />
+        </Flex>
       </Flex>
     </Box>
   );
