abdc0483d217cd083707548b4f2db9eb8ff7740d
jasonhermawan <jasonhs.dev@gmail.com>
2025-06-21 12:01:38 +0700

    feat: add redirect to issue certificate

--- front-end/src/app/dashboard/admin/course/page.tsx
+++ front-end/src/app/dashboard/admin/course/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import CourseDashboard from "@/features/admin/course";
+import { useRouter } from "next/navigation";
 
 const CourseDashboardPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard/admin/course/issue-certificate");
+  }, []);
   return <CourseDashboard />;
 };
 
